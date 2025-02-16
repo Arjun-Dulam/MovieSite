f66dea6d6b26d11933532e7e19c948b6915f2d07
Arjun Dulam <adulam3@gatech.edu>
2025-02-16 05:02:29 +0000

    trying for third time

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # Set the settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', MovieSite.settings')
 
 # Create the WSGI application
 application = get_wsgi_application()
+
+if __name__ == "__main__":
+    from gunicorn.app.wsgiapp import run
+    run()
